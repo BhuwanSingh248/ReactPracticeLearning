e2303482e0a51b910577eab0a32fd2e37c195211
bhuwan.panora <bhuwan.panora@evalueserve.com>
2024-01-20 02:23:00 +0530

    added color form bg changer

--- bgChanges/src/App.jsx
+++ bgChanges/src/App.jsx
@@ -4,11 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [color, setColor] = useState('olive')
   return (
     <>
-     <h1 className='bg-green-500 text-3xl'> background changer app with vite</h1>
+     <div className='w-full h-screen duration-200' style={{backgroundColor:color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg px-3 py-1 rounded-3xl' style={{backgroundColor:color}}>
+          <button onClick={() => setColor('Red')} className='outline-none px-4 py-2 rounded-full bg-red-800 shadow-lg text-black'>Red</button>
+          <button onClick={() => setColor('Green')} className='outline-none px-4 py-2 rounded-full bg-green-800 shadow-lg text-black'>Green</button>
+          <button onClick={() => setColor('Blue')} className='outline-none px-4 py-2 rounded-full bg-blue-800 shadow-lg text-black'>Blue</button>
+        </div>
+      </div>
+     </div>
     </>
   )
 }
